cd08f66cdc1e445a61834e0828a3c450bde37ba9
Pablo Zaidenvoren <pablozaiden@users.noreply.github.com>
2025-07-07 11:19:24 -0300

    Add sleep command to auth.sh for DNS record update delay

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-[[ "$(curl -s "https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN%.duckdns.org}&token=${DUCKDNS_TOKEN}&txt=${CERTBOT_VALIDATION}")" = "OK" ]]
+
+# Make duckdns request to update the DNS record for the domain
+# using the token and validation provided by Certbot.
+response=$(curl -s "https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN%.duckdns.org}&token=${DUCKDNS_TOKEN}&txt=${CERTBOT_VALIDATION}")
+
+# Verify if the response is "OK"
+if [[ "$response" = "OK" ]]; then
+    # DNS record updated successfully, waiting 10 seconds for propagation...
+    sleep 10
+    exit 0
+else
+    # Failed to update DNS record.
+    exit 1
+fi
